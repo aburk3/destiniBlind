b120b8fddcf7e5f21945799ff0173ebff425222b
aburk3 <aburk3@gmail.com>
2018-07-25 21:31:59 -0500

    customizes it all

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -11,58 +11,93 @@ import UIKit
 class ViewController: UIViewController {
 
     // Our strings
-    let story1 = "Alex's car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. Alex decides to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
-    let answer1a = "I\'ll hop in. Thanks for the help!"
-    let answer1b = "Better ask him if he\'s a murderer first."
+    let story1 = "You\'re curious if it is time to file a Complaint with a Court. \nHave you received a Notice of Dismissal and Suit Rights document from the EEOC?"
+    let answer1a = "No, I haven\'t received a Notice of Dismissal or Suit Rights as of yet."
+    let answer1b = "Yes, I have a stamped Notice of Dismissal and Suit Rights Document."
     
     
-    let story2 = "He nods slowly, unphased by the question."
-    let answer2a = "At least he\'s honest. I\'ll climb in."
-    let answer2b = "Wait, I know how to change a tire."
-    
-    let story3 = "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box."
-    let answer3a = "I love Elton John! Hand him the cassette tape."
-    let answer3b = "It\'s him or me! You take the knife and stab him."
-    
-    let story4 = "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?"
-    let story5 = "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in."
-    let story6 = "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: \"Try the pier.\""
+    let story2 = "Great! You are ready to sue then! \nHave you scanned in all documents received from the EEOC AND the envelope?"
+    let answer2a = "I've scanned everything except the envelope. I will scan it as well though. What is next?"
+    let answer2b = "Yes everything is all scanned in!"
+    let story3 = "Well, since you have yet to receive the Notice of Suit Rights and made sure that it was not in Smokeball or Sharepoint, your only step left is to wait for the EEOC to issue it!"
+    let story5 = "Is the Matter on Smokeball changed to Pre-Litigation status and assigned to Allen and Austin?"
+    let answer5a = "Yes, I've assigned the Matter to Allen and Austin as well as changing it out of the EEOC and into the Pre-Litigation Stage."
+    let answer5b = "No, I didn't know about that. Am I supposed to?"
+    let story6 = "Awesome! Well you are on top of things. Go ahead and either hand the papers to Austin so that he can get them to Allen."
+    let story7 = "Yes! Go ahead and change the Smokeball Matter to Pre-Litigation by pressing the top right Plus sign, and then assign it to AB and ADA."
     
     
-    // UI Elements linked to the storyboard
-    @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
-    @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
+    
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var bottomButton: UIButton!
     @IBOutlet weak var storyTextView: UILabel!
+    @IBOutlet weak var restartButton: UIButton!
     
-    // TODO Step 5: Initialise instance variables here
-    
+  
+    var storyIndex:Int = 1
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        topButton.setTitle(answer1a, for: .normal)
-        bottomButton.setTitle(answer1b, for: .normal)
-        
-        
-        // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
+      
+        restart()
         
     }
 
     
     // User presses one of the buttons
     @IBAction func buttonPressed(_ sender: UIButton) {
+        if sender.tag == 1 && storyIndex == 1 {
+            storyTextView.text = story3
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+            storyIndex = 3
+        } else if sender.tag == 2 && storyIndex == 1 {
+            storyTextView.text = story2
+            topButton.setTitle(answer2a, for: .normal)
+            bottomButton.setTitle(answer2b, for: .normal)
+            storyIndex = 2
+        } else if sender.tag == 1 && storyIndex == 2 {
+            storyTextView.text = story5
+            topButton.setTitle(answer5a, for: .normal)
+            bottomButton.setTitle(answer5b, for: .normal)
+            storyIndex = 5
+        } else if (sender.tag == 2 || sender.tag == 1) && storyIndex == 2 {
+            storyTextView.text = story5
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+            storyIndex = 4
+        } else if sender.tag == 1 && storyIndex == 5 {
+            storyTextView.text = story6
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+            storyIndex = 6
+        } else if sender.tag == 2 && storyIndex == 5 {
+            storyTextView.text = story7
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+            storyIndex = 7
+        }
+
     
-        // TODO Step 4: Write an IF-Statement to update the views
-                
-        // TODO Step 6: Modify the IF-Statement to complete the story
-        
-    
+    if storyIndex == 3 || storyIndex == 4 || storyIndex == 6 || storyIndex == 7 {
+        restartButton.isHidden = false
+        }
     }
     
-
-
+    @IBAction func restartPressed(_ sender: UIButton) {
+        restart()
+    }
+    func restart() {
+        restartButton.isHidden = true
+        storyIndex = 1
+        storyTextView.text = story1
+        topButton.setTitle(answer1a, for: .normal)
+        bottomButton.setTitle(answer1b, for: .normal)
+        topButton.isHidden = false
+        bottomButton.isHidden = false
+    }
 
 }
 
